7d3633323fdc3eb675931dec4c0c62d4455ce630
Kevin Lindsey <kevin@kevlindev.com>
2011-03-20 18:23:54 -0500

    Track state changes for push button and rotate active digit only on negative edge.

--- display_counter.vhd
+++ display_counter.vhd
@@ -39,6 +39,8 @@ architecture Behavioral of DisplayCounter is
 	signal up, down: std_logic;
 	signal enable_write: std_logic := '1';
 	signal active_digit: std_logic_vector(3 downto 0) := "1000";
+	signal curr_PB: std_logic := '1';
+	signal last_PB: std_logic := '1';
 begin
 	-- create rotary decoder
 	r: Rotary port map(
@@ -61,26 +63,29 @@ begin
 	);
 	
 	-- update count based on rotary output
-	update_counter: process(clock, up, down)
+	update_counter: process(clock, up, down, PB, active_digit)
 	begin
 		if clock'event and clock = '1' then
+			last_PB <= curr_PB;
+			curr_PB <= PB;
 			if down = '1' then
 				counter <= counter - 1;
 				enable_write <= '1';
 			elsif up = '1' then
 				counter <= counter + 1;
 				enable_write <= '1';
+			elsif curr_PB = '0' and last_PB = '1' then
+				case active_digit is
+					when "0000" => active_digit <= "0001";
+					when "0001" => active_digit <= "0010";
+					when "0010" => active_digit <= "0100";
+					when "0100" => active_digit <= "1000";
+					when "1000" => active_digit <= "0000";
+					when others => active_digit <= "0000";
+				end case;
+				enable_write <= '1';
 			else
 				enable_write <= '0';
-			end if;
-		end if;
-	end process;
-	
-	update_active_digit: process(clock, PB)
-	begin
-		if clock'event and clock = '1' then
-			if PB = '0' then
-				active_digit <= active_digit + 1;
 			end if;
 		end if;
 	end process;

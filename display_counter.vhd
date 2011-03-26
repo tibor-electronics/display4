b079744c61a36395ed1d535fec7a52238a0383dd
Kevin Lindsey <kevin@kevlindev.com>
2011-03-25 22:56:49 -0500

    First pass a adding ability to edit individual digits at a time. This is mostly working. The main issue right now is that if you change a digit's value, it changes when advancing to the next digit.

--- display_counter.vhd
+++ display_counter.vhd
@@ -33,7 +33,7 @@ architecture Behavioral of DisplayCounter is
 		);
 		port(
 			clock: in std_logic;
-			DWE: in std_logic;
+			data_write_enable: in std_logic;
 			data: in std_logic_vector(DIGIT_COUNT * 4 - 1 downto 0);
 			dps: in std_logic_vector(DIGIT_COUNT - 1 downto 0);
 			segments: out std_logic_vector(6 downto 0);
@@ -51,10 +51,15 @@ architecture Behavioral of DisplayCounter is
 		);
 	end component;
 	
-	signal counter: std_logic_vector(15 downto 0) := (others => '0');
+	subtype digitType is std_logic_vector(3 downto 0);
+	type digitTypes is array(0 to 3) of digitType;
+	signal digits: digitTypes;
+	signal display_data: std_logic_vector(15 downto 0);
+	
 	signal up, down: std_logic;
 	signal enable_write: std_logic := '1';
-	signal active_digit: std_logic_vector(3 downto 0) := "1000";
+	signal active_digit: std_logic_vector(3 downto 0) := (others => '0');
+	
 	signal last_debounced: std_logic := '0';
 	signal current_debounced: std_logic := '0';
 	signal PB_debounced: std_logic := '0';
@@ -76,8 +81,8 @@ begin
 		)
 		port map(
 			clock => clock,
-			DWE => enable_write,
-			data => counter,
+			data_write_enable => enable_write,
+			data => display_data,
 			dps => active_digit,
 			segments => segments,
 			dp => dp,
@@ -93,26 +98,34 @@ begin
 			d_out => PB_debounced
 		);
 	
+	update_display: process(clock, digits)
+	begin
+		if clock'event and clock = '1' then
+			display_data <= digits(0) & digits(1) & digits(2) & digits(3);
+		end if;
+	end process;
+	
 	-- update count based on rotary output
 	update_counter: process(clock, up, down, PB, active_digit)
+		variable digit_index: integer range 0 to 3 := 0;
 	begin
 		if clock'event and clock = '1' then
 			last_debounced <= current_debounced;
 			current_debounced <= PB_debounced;
-			if down = '1' then
-				counter <= counter - 1;
+			if down = '1' and active_digit /= "0000" then
+				digits(digit_index) <= digits(digit_index) - 1;
 				enable_write <= '1';
-			elsif up = '1' then
-				counter <= counter + 1;
+			elsif up = '1' and active_digit /= "0000" then
+				digits(digit_index) <= digits(digit_index) + 1;
 				enable_write <= '1';
 			elsif last_debounced = '0' and current_debounced = '1' then
 				case active_digit is
-					when "0000" => active_digit <= "0001";
-					when "0001" => active_digit <= "0010";
-					when "0010" => active_digit <= "0100";
-					when "0100" => active_digit <= "1000";
-					when "1000" => active_digit <= "0000";
-					when others => active_digit <= "0000";
+					when "0000" => active_digit <= "0001"; digit_index := 0;
+					when "0001" => active_digit <= "0010"; digit_index := 1;
+					when "0010" => active_digit <= "0100"; digit_index := 2;
+					when "0100" => active_digit <= "1000"; digit_index := 3;
+					when "1000" => active_digit <= "0000"; digit_index := 0;
+					when others => active_digit <= "0000"; digit_index := 0;
 				end case;
 				enable_write <= '1';
 			else
